9fbe3aec3d40b18a37954f3e21b92a80b943e542
waiyantun <waiyan.web@gmail.com>
2022-07-12 09:51:38 +0630

    color,width,height from setting.py file

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,17 +2,19 @@ import sys
 
 import pygame
 
+from settings import Settings as set
+
 class AlienInvasion:
     """Somethings"""
     def __init__(self):
         """Somethings"""
         pygame.init()
+        self.settings = set()
 
-        self.screen = pygame.display.set_mode((1000,600))
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        #set the background color
-        self.bg_color = (250,200,255)
+        
 
     def run_game(self):
         """Somethings"""
@@ -23,7 +25,7 @@ class AlienInvasion:
                     sys.exit()
                 
                 #Redraw the screen during eash pass through the loop
-                self.screen.fill(self.bg_color)
+                self.screen.fill(self.settings.bg_color)
             
                 pygame.display.flip()
             
