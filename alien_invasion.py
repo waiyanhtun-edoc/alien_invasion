5b3e5a6ea22e20666952a9cce3b2df0b9afbb9aa
waiyantun <waiyan.web@gmail.com>
2022-07-12 09:24:15 +0630

    set backgound color

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,6 +11,9 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1000,600))
         pygame.display.set_caption("Alien Invasion")
 
+        #set the background color
+        self.bg_color = (250,200,255)
+
     def run_game(self):
         """Somethings"""
         while True:
@@ -19,6 +22,8 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
                 
+                #Redraw the screen during eash pass through the loop
+                self.screen.fill(self.bg_color)
             
                 pygame.display.flip()
             
