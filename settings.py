0d40756af7371a6390480ebf1b15b744cf4c6c6f
waiyantun <waiyan.web@gmail.com>
2022-07-14 02:57:41 +0630

    start here

--- settings.py
+++
@@ -1,8 +0,0 @@
-class Settings:
-    """Somethings"""
-    def __init__(self):
-        """Somethings"""
-        #screen settings
-        self.screen_width = 1000
-        self.screen_height = 600
-        self.bg_color = (250,200,255)
