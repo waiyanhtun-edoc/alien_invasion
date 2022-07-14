162b496c3682aeb230d3aa055f983d2918acc82f
waiyantun <waiyan.web@gmail.com>
2022-07-14 07:31:10 +0630

    add module from settings.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ from platform import python_branch
 import sys
 from tkinter import W
 from turtle import screensize
+from settings import Settings
 
 import pygame
 
@@ -12,20 +13,18 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game,and create game resource"""
         pygame.init()
+        self.settings = Settings()
 
         #set the screen size and set the caption
-        self.screen = pygame.display.set_mode((1000,600))
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("ငါလိုးပိုင်ဂိမ်းဟ")
-
-        #setbackground color
-        self.bg_color = (172, 125, 136)
 
     def run_game(self):
         """Start the main loop for game"""
         while True:
 
             #background color
-            self.screen.fill((self.bg_color))
+            self.screen.fill((self.settings.bg_color))
 
             #Watch for keyboard and mouse events
             for event in pygame.event.get():
