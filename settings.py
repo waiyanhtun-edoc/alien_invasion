d444aecf0b2f84edf709ed538fe5e793c6cd0913
waiyantun <waiyan.web@gmail.com>
2022-07-14 07:27:29 +0630

    adding settings module

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """A class all settings for alien_invasion"""
+
+    def __init__(self):
+        """Initialize the game settings"""
+
+        #screen Settings
+        self.screen_width = 1000
+        self.screen_height = 600
+        self.bg_color = (172, 125, 136)
