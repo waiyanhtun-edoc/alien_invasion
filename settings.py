cd807ae4f9eb3e0c902b7a1d3d621d097a01c83f
waiyantun <waiyan.web@gmail.com>
2022-07-16 15:58:04 +0630

    add bullet settings in setting.py file

--- settings.py
+++ settings.py
@@ -11,3 +11,9 @@ class Settings:
         self.caption =("ငါလိုးပိုင်ဂိမ်းဟ")
         self.icon = pygame.image.load("images/icon.png")
         self.ship_speed = 1.5
+
+        #bullet Settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60 ,60 ,60)
